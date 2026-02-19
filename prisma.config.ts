37c796921d0250cacad92d6e0db3ed361b6ae39d
dtgosh <dtg8@naver.com>
2026-02-20 01:09:39 +0900

    chore: 프리즈마관련 파일들을 libs/db로 이동

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'prisma/config';
 import { validatedEnv } from './libs/config/src/config.constants';
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: { path: 'prisma/migrations' },
+  schema: 'libs/db/schema.prisma',
+  migrations: { path: 'libs/db/migrations' },
   datasource: { url: validatedEnv.DATABASE_URL },
 });
